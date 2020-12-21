671e5e9857daf518f0b6a1baa456ed5d426d58f3
Ryez <nevermor2578@gmail.com>
2020-12-21 08:52:48 +0700

    fix all module

--- cat.js
+++ cat.js
@@ -1,8 +1,15 @@
-var cat = {name: 'meow', age: 1};
+//git init : Tạo project
+// git status : xem co su thay doi nao khong.
+//git add *** : Them file ***.
+//git commit -m 'status': xac nhan them voi mess 'status'
+//gi diff // xem noi dung su thay doi cua modifile(nhung file da tao roi)
 
-function callCat()
+
+
+function Cat()
 {
-    console.log(cat);
+    stomat = [];
 }
 
-module.exports= cat;
+
+module.exports= Cat;
--- mouse.js
+++ mouse.js
@@ -1,6 +1,10 @@
-function mouse(name)
+function Mouse(name)
 {
     this.name = name;
+    this.live = false;
 }
-
+Mouse.pototype.die = function()
+{
+    this.live = true;
+}
 module.exports = Mouse; 
